31407c950a1c8b3e8203ff0597f23006a581b4b4
feb day <drons_dron@mail.ru>
2021-08-12 18:52:28 +0400

    fix bug

--- utils/utils_tictactoe.py
+++ utils/utils_tictactoe.py
@@ -3,4 +3,4 @@ import json
 
 async def checked_click_button(place, button):  # Проверяет является ли место куда кликнули занятым
 	button = button.split("plane_")[1]
-	return place[int(button)] == " "	
+	return place[int(button)] != "🔥"
